6c0083c5e1792a3973b2551bfe211cbdf5ce5f57
yazakhariya <17070501.iana@gmail.com>
2023-06-22 00:49:10 +0300

    volume func set

--- src/screens/mainPageScreen/mainPageComponents/bar/volume-content.jsx
+++ src/screens/mainPageScreen/mainPageComponents/bar/volume-content.jsx
@@ -5,11 +5,17 @@ import s from './volume-content.module.css';
 
 function VolumeContent({ controls }) {
 
+    const volumeChange = (e) => {
+        const range = e.target;
+        const rangeValue = Number(range.value) / 100;
+        controls.volume(rangeValue);
+    }
+
     return (
         <div className={s.volume__content}>
             <ReactSVG src={volume} className={s.volume__image} alt="volume" />
             <div className={`${s.volume__progress} ${s._btn}`}>
-                <InputForm onChange={() => controls.volume(.1)} className={`${s.volume__progress_line} ${s._btn}`} type="range" name="range" />
+                <InputForm min="0" max="100" step="any" onChange={volumeChange} className={`${s.volume__progress_line} ${s._btn}`} type="range" name="range" />
             </div>
         </div>  
     )
