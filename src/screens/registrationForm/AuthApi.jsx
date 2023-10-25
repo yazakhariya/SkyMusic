688c528124937354155011a7fac0995decabed2a
yazakhariya <17070501.iana@gmail.com>
2023-10-25 23:19:43 +0300

    feature: api changed

--- src/screens/registrationForm/AuthApi.jsx
+++ src/screens/registrationForm/AuthApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:'https://painassasin.online/',
+        baseUrl:'https://skypro-music-api.skyeng.tech/',
 
     }),
     endpoints: (builder) => ({
